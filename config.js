e40d9d6190b281a27cc70be54b9916648f965b91
Farhan <theinternetbae@gmail.com>
2017-08-21 20:52:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  consumer_key:         'qRDUea8d2IMZzJBGspxoJwrpj',
-  consumer_secret:      'luv7yPBCdB7aZMzH69tqJ5qcwa7vdX5V3EYLWnqFoLfRYTT6z8',
-  access_token:         '898956704833982464-KsoKwor5EcPNjXmJoNGFHy39yLrhTCA',
-  access_token_secret:  'MoK29AXYO5k8IFJJ3nqOD3kAvFI0p0BKGQcffenMQWEay'
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  ''
 }
